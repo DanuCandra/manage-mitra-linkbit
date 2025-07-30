fe02c61c5b15d4beee9b6ae0e8ad7c9ba0b81b5a
Danu Candra Saputra <danucahndx33@gmail.com>
2025-07-30 18:03:42 +0700

    first commit

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,17 +18,24 @@ class UserSeeder extends Seeder
             'name' => 'Super Admin',
             'email' => 'superadmin@gmail.com',
             'password' => bcrypt('123'),
+            'status' => 'active',
+            'role_id' => 1,
             ],
             [
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('123'),
+            'status' => 'active',
+            'role_id' => 2,
             ],
             [
             'name' => 'Mitra',
             'email' => 'mitra@gmail.com',
             'password' => bcrypt('123'),
+            'status' => 'active',
+            'role_id' => 3,
             ],
+            
         ];
 
         DB::table('users')->insert($users);
