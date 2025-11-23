f504377bc70480e67db844b932d4ae134087feac
Danu Candra Saputra <danucahndx33@gmail.com>
2025-11-23 23:51:50 +0700

    tambah managemen bandwidth admin

--- resources/views/mitra/profile-mitra/view-profile.blade.php
+++ resources/views/mitra/profile-mitra/view-profile.blade.php
@@ -79,7 +79,7 @@
                             <div class="form-group row">
                                 <label class="form-label text-end col-md-3">Bandwidth:</label>
                                 <div class="col-md-9">
-                                    <p>{{ $mitra->bandwith }}</p>
+                                    <p>{{ $mitra->bandwidth }}</p>
                                 </div>
                             </div>
                         </div>
